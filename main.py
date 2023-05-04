076729b2077583ffbae80c9f05acefe233d0e621
pespinel <pabloecab@icloud.com>
2023-05-04 10:05:02 +0200

    Fixes

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from captcha import captcha_solver
 from flow import flow
 from setup import setup
 import utils.constants as constants
@@ -6,6 +5,7 @@ import utils.constants as constants
 import csv
 import logging
 from selenium.common.exceptions import ElementNotVisibleException
+from time import sleep
 
 logger = logging.basicConfig(filename=constants.LOG_NAME, level=logging.INFO)
 
@@ -13,22 +13,23 @@ logger = logging.basicConfig(filename=constants.LOG_NAME, level=logging.INFO)
 if __name__ == "__main__":
     driver_wrapper = setup()
 
-    with open(constants.CSV_NAME) as file:
-        data = csv.reader(file)
+    with open(constants.CSV_NAME, encoding=constants.ENCODING) as file:
+        data = csv.reader(file, skipinitialspace=True)
         for row in data:
             completed = False
-            username, password = row[0].split(constants.CSV_DELIMITER)
+            username = row[0].split(constants.CSV_DELIMITER)[
+                0].replace('\ufeff', '')
+            password = row[0].split(constants.CSV_DELIMITER)[
+                1].replace('\ufeff', '')
             while not completed:
                 try:
                     flow(driver_wrapper.driver, username, password)
-                    logging.info("Account created: {}".format(username))
-                except ElementNotVisibleException:
-                    logger.error("Error creating account: {}".format(username))
-                    captcha_solver(driver_wrapper.driver)
+                except Exception:
                     driver_wrapper.close()
                     driver_wrapper = setup()
                 else:
                     completed = True
+                    logging.info("Account created: {}".format(username))
                 finally:
                     driver_wrapper.driver.delete_all_cookies()
                     driver_wrapper.driver.refresh()
